bf5e1c9393f97a668af404002e591d24d20c7fb2
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2021-05-11 10:52:30 +0300

    OOP and code organization

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import sys
 import math
+from abc import ABC, abstractmethod
+
+
+""" ---------------
+      Consts
+--------------- """
 
 BOOST = "BOOST"
-boost_limit = 1
-checkpoint_radius = 600
-
-self_amount = 0
-enemy_amount = 0
-
-points = []
-
-self_history = []
-opponent_history = []
-
-def compute_distance(point1, point2):
-    return math.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2)
+BOOST_LIMIT = 1
+CHECKPOINT_RADIUS = 600
 
 
-class Point:
-    def __init__(self, x, y):
+""" ---------------
+     Classes
+--------------- """
+
+class Object(ABC):
+    def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
 
@@ -26,55 +25,101 @@ class Point:
         return "({}, {})".format(self.x, self.y)
 
     def __eq__(self, other):
-        if isinstance(other, Point):
+        if isinstance(other, Object):
             return self.x == other.x and self.y == other.y
         return False
 
-current = -1
-unfill = True
+    def tuple(self):
+        return self.x, self.y
 
+
+class Location(Object):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+
+
+class CheckPoint(Location):
+    def __init__(self, x: int, y: int, index: int):
+        super().__init__(x, y)
+        self.index = index
+
+
+class Pod:
+    def __init__(self, location: Location, index: int):
+        self.loc = location
+        self.index = index
+        self.current = -1
+
+    def x(self):
+        return self.loc.x
+
+    def y(self):
+        return self.loc.y
+
+
+""" ---------------
+      Methods
+--------------- """
+
+def compute_distance(current: (int, int), other: (int, int)):
+    return math.sqrt((current[0] - other[0]) ** 2 + (current[1] - other[1]) ** 2)
+
+
+""" ---------------
+Game Loop Variables
+--------------- """
+
+self = Pod(None, 0)
+opp = Pod(None, 0)
+next = None
+points = []
+filled = False
+
+
+""" ---------------
+     Game Loop
+--------------- """
 
 while True:
     x, y, next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, next_checkpoint_angle = [int(i) for i in input().split()]
     opponent_x, opponent_y = [int(i) for i in input().split()]
     thrust = 100
 
-    cur_loc = Point(x, y)
-    next_loc = Point(next_checkpoint_x, next_checkpoint_y)
-    opp_loc = Point(opponent_x, opponent_y)
+    self.loc = Location(x, y)
+    opp.loc = Location(opponent_x, opponent_y)
+    next = CheckPoint(next_checkpoint_x, next_checkpoint_y, -1)
 
-    if not unfill:
-        if points.index(next_loc) != (current + 1) % len(points):
-            current += 1
-            current %= len(points)
+    if filled:
+        if points.index(next.tuple()) != (self.current + 1) % len(points):
+            self.current += 1
+            self.current %= len(points)
 
     sys.stderr.write("points: " + str(list(map(str, points))) + "\n")
-    sys.stderr.write("current: " + str(current))
+    sys.stderr.write("current: " + str(self.current))
 
-    if unfill:
+    if not filled:
         if len(points) == 0:
-            points.append(next_loc)
-        elif next_loc != points[-1] and next_loc != points[0]:
-            points.append(next_loc)
-        elif next_loc != points[-1] and next_loc == points[0]:
-            unfill = False
-            current = 0
+            points.append(next.tuple())
+        elif next.tuple() != points[-1] and next.tuple() != points[0]:
+            points.append(next.tuple())
+        elif next.tuple() != points[-1] and next.tuple() == points[0]:
+            filled = True
+            self.current = 0
             points = [points[-1]] + points[:len(points) - 1]
 
-
-    if boost_limit > 0:
-        if not unfill:
+    if BOOST_LIMIT > 0:
+        if filled:
             distances = []
             for i in range(len(points) - 1):
                 dist = compute_distance(points[i], points[i + 1])
                 distances.append(dist)
             m = distances.index(max(distances))
 
-            if current == m and -15 < next_checkpoint_angle < 15:
+            if self.current == m and -15 < next_checkpoint_angle < 15:
                 thrust = BOOST
-                boost_limit -= 1
+                BOOST_LIMIT -= 1
 
     if next_checkpoint_angle > 45 or next_checkpoint_angle < -45:
         thrust = 0
 
-    print("{} {} {}".format(str(next_loc.x), str(next_loc.y), thrust))
+    print("{} {} {}".format(str(next.x), str(next.y), thrust))
